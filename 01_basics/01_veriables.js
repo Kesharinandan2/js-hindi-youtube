f99380cb05b93980aae819050f78b6958e3dc53e
Kesharinandan2 <kesharinandanjha1@gmail.com>
2024-05-14 15:48:17 +0000

    dattatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmaill = "kesharinandanjha1@gmail.com"
+var accountPass = "12345"
+accountCity = "Barabanki"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "kc@gmail.com"
+accountPass = "212121"
+accountCity = "Lucknow"
+
+console.log(accountId);
+
+//prefeer not to use var becouse of issue in block scope and functional scope//
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+console.log(3 + 3) 
+
+console.log("Keshari")
+
+let name = "Keshari" //string datatype
+let age = 18 //number
+let isloggedIn = false //boolean datatype
+
+
+// number => 2 to power 53
+//bigInt for big big number system just like facebook 
+// string = ""
+// boolean => true/false
+// null => standalone value (empty)
+// undefined => value not defined
+// symbol => unique
+
+// object
+
+console.log(typeof "Keshari");
+console.log(typeof undefined); //output undefined
+console.log(typeof null); //output object
